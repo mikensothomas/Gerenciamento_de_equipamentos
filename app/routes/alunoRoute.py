51040648498c8558831eeadd77e383a6a1f68c5b
Mikensonthomas142 <mikensonthomas2@gmail.com>
2026-07-22 19:52:31 -0300

    Ajusta o POST

--- app/routes/alunoRoute.py
+++ app/routes/alunoRoute.py
@@ -1,22 +1,24 @@
 from fastapi import APIRouter
 from DB import db
-from entidades import Aluno
+# from entidades import Aluno
 
 alunoRoutes = APIRouter()
-database = db.Database
+database = db.Database()
 
 @alunoRoutes.get("/alunos")
 def getAlunoData():
     return database.getData()
 
-@alunoRoutes.post("/aluno")
+@alunoRoutes.post("/alunos")
 def inserAluno(parametro:dict):
-    
-    aluno = Aluno()
 
-    aluno.id(parametro.get("id"))
-    aluno.nome(parametro.get("nome"))
-    aluno.numeroMatricula(parametro.get("numeroMatricula"))
-    aluno.curso(parametro.get("curso"))
-    aluno.email(parametro.get("email"))
-    database.insert(aluno.id, aluno)
+    # aluno = Aluno()
+
+    id = parametro.get("id")
+    # aluno.id(parametro.get("id"))
+    # aluno.nome(parametro.get("nome"))
+    # aluno.numeroMatricula(parametro.get("numeroMatricula"))
+    # aluno.curso(parametro.get("curso"))
+    # aluno.email(parametro.get("email"))
+    # database.insert(aluno.id, aluno)
+    database.insert(id, parametro)

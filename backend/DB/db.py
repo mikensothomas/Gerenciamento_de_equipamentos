6b8f2ddad10c91515e66bb85233355fc240d6605
Mikensonthomas142 <mikensonthomas2@gmail.com>
2026-08-17 23:13:39 -0300

    Conexão com DB

--- backend/DB/db.py
+++ backend/DB/db.py
@@ -1,18 +1,28 @@
+from sshtunnel import SSHTunnelForwarder
+from config.Config import Settings
+from sqlmodel import create_engine, Session
+
 class Database:
     def __init__(self):
-        self.index = 0
-        self.banco = {}
+        self._engine = None
+        self._tunel = None
 
-    def insert(self, id, object):
-        self.banco[id] = object
-    
-    def getData(self):
-        return self.banco
-    
-    def updateData(self, id):
-        pass
+    def starTunnel(self):
+        self._tunel = SSHTunnelForwarder(
+            (Settings.ssh_host, Settings.ssh_port),
+            ssh_username=Settings.ssh_user,
+            ssh_password=Settings.ssh_password,
+            remote_bind_address=(Settings.db_host, Settings.db_port)
+        )
+        self._tunel.start()
 
-    def next_index(self):
-        self.index += 1
+        local_port = self._tunel.local_bind_port
+        url = f"mysql+pymysql://{Settings.db_user}:{Settings.db_password}@127.0.0.1:{local_port}/{Settings.db_name}"
+        
+        self._engine = create_engine(url, echo=True)
 
-        yield self.index
+    def get_db(self):
+        if not self._engine:
+            raise RuntimeError("Engine não foi inicializada!")
+        with Session(self._engine) as s:
+            yield s
--- backend/config/Config.py
+++ backend/config/Config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     ssh_host: str
-    ssh_port: int = 22
+    ssh_port: int = 3306
     ssh_user: str
-    ssh_pasword: str | None = None
+    ssh_password: str | None = None
 
     db_host: str = "127.0.0.1"
     db_port: int = 3306
